58c13e18ea907e3a308f224bdf23da05beea3169
Tariq Khan <71902492+tariq-k-dev@users.noreply.github.com>
2024-07-14 12:37:33 -0700

    finish lab 01

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,15 @@
 import arcade
 
 print('The installed version of the Arcade package is:', arcade.__version__)
+
+print("""
+This is an example of 
+using triple quotes to 
+print multiple lines
+""")
+
+print("Printing a double quote within a double quote \" with \\\".")
+print('Printing a single \' quote within single quotes with \\\'.')
+print("\tTabbed using \\t")
+print("First line\nNext line using \\n")
+print("\r Printing with \\r")
